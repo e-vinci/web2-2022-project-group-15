0297e247ebe9f5620c4595f063eb59fe1c081bd3
JDRjuju <94639280+JDRjuju@users.noreply.github.com>
2022-12-06 12:07:19 +0100

    added: auths login

--- frontend/src/Components/Pages/loginPage.js
+++ frontend/src/Components/Pages/loginPage.js
@@ -1,4 +1,6 @@
+import { setAuthenticatedUser } from '../../utils/auths';
 import { clearPage, renderPageTitle } from '../../utils/render';
+
 
 const LoginPage = () => {
   clearPage();
@@ -30,6 +32,39 @@ function renderRegisterForm() {
   form.appendChild(password);
   form.appendChild(submit);
   main.appendChild(form);
+  form.addEventListener('submit', onLogin);
+}
+
+async function onLogin(e) {
+  e.preventDefault();
+
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const response = await fetch('/api/auths/login', options);
+
+  if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+
+  const authenticatedUser = await response.json();
+
+  console.log('Authenticated user : ', authenticatedUser);
+
+  setAuthenticatedUser(authenticatedUser);
+
+  // Navbar();
+
+  // Navigate('/');
 }
 
 export default LoginPage;

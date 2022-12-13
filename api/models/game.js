4f6d82403c2017a21bb655fe2726d0e18d397241
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2022-12-13 12:14:59 +0100

    Update game.js

--- api/models/game.js
+++ api/models/game.js
@@ -1,10 +1,10 @@
+const unlocks = [5,10,15];
+
+let  resources=[0,0,0,0];
+
 let lvl = 0;
-const unlock2 = 5;
-const unlock3 = 10;
-const unlock4 = 15;
-let money=0
-let  resource=[0,0,0,0]
-let moneyToLvlUp=0
+let money = 0;
+let moneyToLvlUp = 0;
 
 // let money = rc1*5 + rc2*20 + rc3*100 + rc4*250;
 
@@ -23,11 +23,11 @@ function getlvl(){
 }
 
 function getnbreressource1( number){
-    if(resource <1 || resource>4){throw Error}else{return resource[number];}
+    if(number <1 || number>4){throw Error}else{return resources[number];}
 }
 
 function resource(number){
-      resource[number] += 1;
+      resources[number] += 1;
   }
   
   

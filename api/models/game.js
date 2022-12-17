6da4dc4adb12fe6a4b92727287f97ac50d8bcf00
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2022-12-17 16:04:42 +0100

    all backend function works

--- api/models/game.js
+++ api/models/game.js
@@ -3,168 +3,208 @@ const path = require('node:path');
 const jsonDbPath = path.join(__dirname, '/../data/players.json');
 const { parse, serialize } = require('../utils/json');
 
-let coal;
-let iron;
-let silver;
-let gold;
+const priceCoal = 5;
+const priceIron = 20;
+const priceSilver = 100;
+const priceGold = 250;
 
-let lvl;
-let money;
-let moneyToLvlUp;
+// CONSTRUCTOR PLAYEUR
 
 const defaultPlayer = [
   {
     playerId: 1,
 
+    lvl: 0,
+
     coal: 0,
     iron: 0,
     silver: 0,
     gold: 0,
 
-    lvl: 0,
     money: 0,
-    moneyToLvlUp: 0,
+    moneyLvlUp: 10,
   },
 ];
 
 function creatPlayer(id){
-
   const list = parse(jsonDbPath, defaultPlayer);
 
   const player = {
     playerId: id,
 
+    lvl: 0,
+
     coal: 0,
     iron: 0,
     silver: 0,
     gold: 0,
 
-    lvl: 0,
     money: 0,
-    moneyToLvlUp: 0,
-  }
+    moneyLvlUp: 10,
+  };
   
   list.push(player);
   serialize(jsonDbPath, list);
 }
 
-// let money = rc1*5 + rc2*20 + rc3*100 + rc4*250;
-
-function setmoneyToLvlUp(id, number){
-
-
-    moneyToLvlUp=number
-}
-
-function getmoneyToLvlUp(){
-    return moneyToLvlUp;
-}
-
-function getmoney(){
-    return money
-}
+// COAL
 
 function getCoal(id){
-  console.log("i'm in");
+  const playerFound = getPlayer(id);
+  return playerFound.coal;
+}
 
+function setCoal(id){
+  const playerFound = getPlayer(id);
+
+  console.log(playerFound.coal);
+
+  let resource = playerFound.coal;
+  resource += 1;
+  playerFound.coal = resource;
+
+  console.log(playerFound.coal);
+}
+
+// IRON
+
+function getIron(id){
+  const playerFound = getPlayer(id);
+  return playerFound.iron;
+}
+
+function setIron(id){
+  const playerFound = getPlayer(id);
+
+  let resource = playerFound.iron;
+  resource += 1;
+  playerFound.iron = resource;
+}
+
+// SILVER
+
+function getSilver(id){
+  const playerFound = getPlayer(id);
+  return playerFound.silver;
+}
+
+function setSilver(id){
+  const playerFound = getPlayer(id);
+
+  let resource = playerFound.silver;
+  resource += 1;
+  playerFound.silver = resource;
+}
+
+// GOLD
+
+function getGold(id){
+  const playerFound = getPlayer(id);
+  return playerFound.gold;
+}
+
+function setGold(id){
+  const playerFound = getPlayer(id);
+
+  let resource = playerFound.gold;
+  resource += 1;
+  playerFound.gold = resource;
+}
+
+// MONEY
+
+function getmoney(id){
+  const playerFound = getPlayer(id);
+  return playerFound.money;
+}
+
+function sellResources(id){
+  const playerFound = getPlayer(id);
+
+  let totalMoney = playerFound.money;
+
+  totalMoney += playerFound.coal * priceCoal;
+  totalMoney += playerFound.iron * priceIron;
+  totalMoney += playerFound.silver * priceSilver;
+  totalMoney += playerFound.gold * priceGold;
+
+  playerFound.coal = 0;
+  playerFound.iron = 0;
+  playerFound.silver = 0;
+  playerFound.gold = 0;
+
+  playerFound.money = totalMoney;
+}
+
+// LVL
+
+function getLvl(id){
+  const playerFound = getPlayer(id);
+  return playerFound.lvl;
+}
+
+function lvlUp(id){
+  const playerFound = getPlayer(id);
+
+  let newLvl = playerFound.lvl;
+
+  const newMoney = playerFound.money;
+  const cost = playerFound.moneyLvlUp;
+  
+  if(cost < newMoney){
+    playerFound.money = newMoney-cost;
+
+    newLvl +=1;
+    playerFound.lvl = newLvl;
+
+    setPriceLvlUp(id);
+  }
+}
+
+// PRICE LVL
+
+function setPriceLvlUp(id){
+  const playerFound = getPlayer(id);
+
+  const price = playerFound.moneyLvlUp;
+  const newPrice = 1.5 * price + 5;
+  playerFound.moneyLvlUp = newPrice;
+}
+
+function getPriceLvlUp(id){
+  const playerFound = getPlayer(id);
+  return playerFound.moneyLvlUp;
+}
+
+// PLAYER  
+
+function getPlayer(id){
   const list = parse(jsonDbPath, defaultPlayer);
   const playerFound = list.find((player) => player.playerId === id);
 
   if(playerFound === undefined){
     throw Error;
   }
-
-  return playerFound.coal
+  return playerFound;
 }
-
-function getIron(id){
-  const list = parse(jsonDbPath, defaultPlayer);
-  const playerFound = list.find((player) => player.playerId === id);
-
-  return playerFound.iron
-}
-
-function getSilver(id){
-  const list = parse(jsonDbPath, defaultPlayer);
-  const playerFound = list.find((player) => player.playerId === id);
-
-  return playerFound.silver
-}
-
-function getGold(id){
-  const list = parse(jsonDbPath, defaultPlayer);
-  const playerFound = list.find((player) => player.playerId === id);
-
-  return playerFound.gold
-}
-
-
-function setCoal(id){
-  const list = parse(jsonDbPath, defaultPlayer);
-  const playerFound = list.find((player) => player.playerId === id);
-
-  coal = playerFound.coal;
-  coal += 1;
-  playerFound.coal = coal;
-}
-
-function setIron(id){
-  const list = parse(jsonDbPath, defaultPlayer);
-  const playerFound = list.find((player) => player.playerId === id);
-
-  iron = playerFound.iron;
-  iron += 1;
-  playerFound.iron = iron;
-}
-
-function setSilver(id){
-  const list = parse(jsonDbPath, defaultPlayer);
-  const playerFound = list.find((player) => player.playerId === id);
-
-  silver = playerFound.silver;
-  silver += 1;
-  playerFound.silver = silver;
-}
-
-function setGold(id){
-  const list = parse(jsonDbPath, defaultPlayer);
-  const playerFound = list.find((player) => player.playerId === id);
-
-  gold = playerFound.gold;
-  gold += 1;
-  playerFound.gold = gold;
-}
-  
-  
-function upHisLvl(id){
-  if(money >= moneyToLvlUp){
-    money -= moneyToLvlUp;
-    lvl += 1;
-    increase();  
-  }
-}
-  
-function increase(id){
-  moneyToLvlUp = lvl*150.5;
-  moneyToLvlUp=Math.round(moneyToLvlUp);
-}
-  
   
 module.exports ={
   creatPlayer,
-  setmoneyToLvlUp,
-  getmoneyToLvlUp,
 
   getCoal,
   getIron,
   getSilver,
   getGold,
+  getmoney,
+  getLvl,
+  getPriceLvlUp,
 
   setCoal,
   setIron,
   setSilver,
   setGold,
 
-  upHisLvl,
-  getmoney
+  sellResources,
+
+  lvlUp,
 }
--- api/routes/game.js
+++ api/routes/game.js
@@ -1,53 +1,100 @@
 const express = require('express');
 
-const { creatPlayer, 
-    setmoneyToLvlUp, 
-    getmoneyToLvlUp, 
-    getCoal, 
-    getIron, 
-    getSilver, 
-    getGold, 
-    setCoal, 
-    setIron, 
-    setSilver, 
-    setGold, 
-    upHisLvl, 
-    getmoney } = require('../models/game');
+const {
+    getCoal,
+    getIron,
+    getSilver,
+    getGold,
+    getmoney,
+    getLvl,
+    getPriceLvlUp,
+
+    setCoal,
+    setIron,
+    setSilver,
+    setGold,
+
+    sellResources,
+
+    lvlUp,
+    } = require('../models/game');
 
 const { getId } = require('../models/users');
 
 const router = express.Router();
 
+// COAL
+
 router.post('/getCoal',(req,res)=>{
-    console.log("rout")
+    console.log("route");
     const result = getCoal(getId());
     return res.json(result);
 });
+
+router.post('/setCoal', (req,res)=>{
+    setCoal(getId());
+});
+
+// IRON
 
 router.post('/getIron',(req,res)=>{
     const result = getIron(getId());
     return res.json(result);
 });
 
+router.post('/setIron', (req,res)=>{
+    setIron(getId());
+});
+
+// SILVER
+
 router.post('/getSilver',(req,res)=>{
     const result = getSilver(getId());
     return res.json(result);
 });
 
-router.post('/getGolde',(req,res)=>{
+router.post('/setSilver', (req,res)=>{
+    setSilver(getId());
+});
+
+// GOLD
+
+router.post('/getGold',(req,res)=>{
     const result = getGold(getId());
     return res.json(result);
-});
-  
+}); 
 
-router.post('/setgame', (req) => {
-    const money = req?.body?.money !== 0 ? req.body.money : undefined;
-  
-    if (money !==undefined){
-         setmoneyToLvlUp(id, money);
-    }
-    
-  });
-  
+router.post('/setGold', (req,res)=>{
+    setGold(getId());
+});
+
+// MONEY
+
+router.post('/getMoney',(req,res)=>{
+    const result = getmoney(getId());
+    return res.json(result);
+}); 
+
+router.post('/sellResources',(req,res)=>{
+    sellResources(getId());
+});
+
+// PRICE
+
+router.post('/getPriceLvlUp',(req,res)=>{
+    const result = getPriceLvlUp(getId());
+    return res.json(result);
+});
+
+// LVL
+
+router.post('/getLvl',(req,res)=>{
+    const result = getLvl(getId());
+    return res.json(result);
+});
+
+router.post('/lvlUp',(req,res)=>{
+    lvlUp(getId());
+});
 
 module.exports=router;
